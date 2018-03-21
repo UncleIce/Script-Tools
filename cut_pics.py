da8882b0e04decfdf54e47bc6ff7cfc2c489bce1
Uncleice <how.young@hotmail.com>
2018-03-21 16:03:28 +0800

    add batch cropping script

---
+++ cut_pics.py
@@ -0,0 +1,35 @@
+#coding=utf-8
+import os
+import pylab as plb
+import PIL.Image as Image
+
+cur_path = os.getcwd() # 获取当前路径
+def getAllImages(path):
+    #f.endswith（）  限制文件类型
+    #f.endswith('.jpg')|f.endswith('.png')  读取jpg/png格式的文件
+    #注意 返回的是绝对路径
+   return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')|f.endswith('.png')]
+#将当前路径下的所有图片path存放进数组
+filelist = [i for i in getAllImages(cur_path)]   
+
+#打开第一张图片 
+pic =  Image.open(filelist[0])  
+plb.imshow(pic)        
+# 设置裁剪点（2个）
+cood = plb.ginput(2)
+# 取点确定裁切坐标
+left = cood[0][0] 
+top =  cood[0][1]
+right = cood[1][0]
+bottom = cood[1][1]
+box = [int(left),int(top),int(right),int(bottom)]
+plb.close()
+   
+for pic in filelist:
+    img = Image.open(pic)
+    #裁剪
+    img2 = img.crop(box)
+    #储存为原来路径覆盖原文件
+    img2.save(pic)
+    
+# plb.show()
